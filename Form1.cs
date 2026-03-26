ff91acd309ddd64d4932f30461257d866b44f6a3
sohyun031031-commits <sohyun031031@gmail.com>
2026-03-26 13:11:39 +0900

    숫자 버튼을 누르면 위 텍스트 박스에 뜨게끔 함

--- Form1.cs
+++ Form1.cs
@@ -2,10 +2,14 @@ namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
+        private double firstNumber = 0;
+        private string operation = "";
+
         public Form1()
         {
             InitializeComponent();
         }
+
         private void HandleNumberInput(string digit)
         {
             if (txtInputWindow.Text == "0")
@@ -26,25 +30,116 @@ namespace SimpleCalculator
         private void btnNum9_Click(object sender, EventArgs e) { HandleNumberInput("9"); }
 
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            firstNumber = double.Parse(txtInputWindow.Text);
+            operation = "+";
+            txtInputWindow.Text += "+";
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void btnSubtract_Click(object sender, EventArgs e)
         {
-
+            firstNumber = double.Parse(txtInputWindow.Text);
+            operation = "-";
+            txtInputWindow.Text += "-";
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void btnMultiply_Click(object sender, EventArgs e)
         {
-            //txtInputWindow.Text = "0";
-
+            firstNumber = double.Parse(txtInputWindow.Text);
+            operation = "×";
+            txtInputWindow.Text += "×";
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void btnDivide_Click(object sender, EventArgs e)
         {
-            //txtOutputWindow.Text = "0";
+            firstNumber = double.Parse(txtInputWindow.Text);
+            operation = "÷";
+            txtInputWindow.Text += "÷";
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            string input = txtInputWindow.Text;
+            double result = 0;
+
+            if (input.Contains("+"))
+            {
+                string[] parts = input.Split('+');
+                result = double.Parse(parts[0]) + double.Parse(parts[1]);
+            }
+            else if (input.Contains("-"))
+            {
+                string[] parts = input.Split('-');
+                result = double.Parse(parts[0]) - double.Parse(parts[1]);
+            }
+            else if (input.Contains("×"))
+            {
+                string[] parts = input.Split('×');
+                result = double.Parse(parts[0]) * double.Parse(parts[1]);
+            }
+            else if (input.Contains("÷"))
+            {
+                string[] parts = input.Split('÷');
+                if (double.Parse(parts[1]) == 0)
+                {
+                    txtOutputWindow.Text = "0으로 나눌 수 없음";
+                    return;
+                }
+                result = double.Parse(parts[0]) / double.Parse(parts[1]);
+            }
+
+            txtInputWindow.Text += "=" + result.ToString();
+            txtOutputWindow.Text = result.ToString();
+        }
+
+        private void btnFuncC_Click(object sender, EventArgs e)
+        {
+            txtInputWindow.Text = "0";
+            txtOutputWindow.Text = "0";
+        }
+
+        private void btnFuncCE_Click(object sender, EventArgs e)
+        {
+            string input = txtInputWindow.Text;
+
+            if (input.Contains("+"))
+            {
+                txtInputWindow.Text = input.Substring(0, input.IndexOf('+') + 1);
+            }
+            else if (input.Contains("-"))
+            {
+                txtInputWindow.Text = input.Substring(0, input.IndexOf('-') + 1);
+            }
+            else if (input.Contains("×"))
+            {
+                txtInputWindow.Text = input.Substring(0, input.IndexOf('×') + 1);
+            }
+            else if (input.Contains("÷"))
+            {
+                txtInputWindow.Text = input.Substring(0, input.IndexOf('÷') + 1);
+            }
+            else
+            {
+                txtInputWindow.Text = "0";
+            }
+        }
+
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+        private void button4_Click(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void btnFuncDel_Click(object sender, EventArgs e)
+        {
+            string input = txtInputWindow.Text;
+
+            if (input.Length > 1)
+                txtInputWindow.Text = input.Substring(0, input.Length - 1);
+            else
+                txtInputWindow.Text = "0";
         }
     }
 }
