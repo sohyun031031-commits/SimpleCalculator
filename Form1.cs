2eff210939eb2df63bc7f6a41a4a12a7c8288cd7
sohyun031031-commits <sohyun031031@gmail.com>
2026-03-26 12:41:10 +0900

    숫자 버튼을 누르면 위 텍스트 박스에 뜨게끔 함

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,25 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+        private void HandleNumberInput(string digit)
+        {
+            if (txtInputWindow.Text == "0")
+                txtInputWindow.Text = digit;
+            else
+                txtInputWindow.Text += digit;
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e) { HandleNumberInput("0"); }
+        private void btnNum1_Click(object sender, EventArgs e) { HandleNumberInput("1"); }
+        private void btnNum2_Click(object sender, EventArgs e) { HandleNumberInput("2"); }
+        private void btnNum3_Click(object sender, EventArgs e) { HandleNumberInput("3"); }
+        private void btnNum4_Click(object sender, EventArgs e) { HandleNumberInput("4"); }
+        private void btnNum5_Click(object sender, EventArgs e) { HandleNumberInput("5"); }
+        private void btnNum6_Click(object sender, EventArgs e) { HandleNumberInput("6"); }
+        private void btnNum7_Click(object sender, EventArgs e) { HandleNumberInput("7"); }
+        private void btnNum8_Click(object sender, EventArgs e) { HandleNumberInput("8"); }
+        private void btnNum9_Click(object sender, EventArgs e) { HandleNumberInput("9"); }
+
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
@@ -19,13 +38,13 @@ namespace SimpleCalculator
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            txtInputWindow.Text = "0";
+            //txtInputWindow.Text = "0";
 
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            txtOutputWindow.Text = "0";
+            //txtOutputWindow.Text = "0";
         }
     }
 }
